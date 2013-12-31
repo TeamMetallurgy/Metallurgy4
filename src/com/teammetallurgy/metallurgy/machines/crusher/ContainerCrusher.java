d617b2fb734b8a092471dee23790d3b13d65dcf9
Freyja <mike.stengel@gmail.com>
2013-12-31 10:22:36 -0500

    Crusher Container and TileEntity Work.

--- src/com/teammetallurgy/metallurgy/machines/crusher/ContainerCrusher.java
+++ src/com/teammetallurgy/metallurgy/machines/crusher/ContainerCrusher.java
@@ -5,13 +5,33 @@ import com.teammetallurgy.metallurgy.machines.TileEntityMetallurgy;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 
 public class ContainerCrusher extends Container
 {
 
+    private TileEntityMetallurgy crusher;
+
     public ContainerCrusher(InventoryPlayer inventoryPlayer, TileEntityMetallurgy tileEntity)
     {
+        this.crusher = tileEntity;
+        addPlayersInventoryToContainer(inventoryPlayer, 8, 84);
+    }
 
+    private void addPlayersInventoryToContainer(InventoryPlayer inventoryPlayer, int xStart, int yStart)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 9; ++j)
+            {
+                this.addSlotToContainer(new Slot(inventoryPlayer, j + i * 9 + 9, xStart + j * 18, yStart + i * 18));
+            }
+        }
+
+        for (int i = 0; i < 9; ++i)
+        {
+            this.addSlotToContainer(new Slot(inventoryPlayer, i, xStart + i * 18, 142));
+        }
     }
 
     @Override
--- src/com/teammetallurgy/metallurgy/machines/crusher/TileEntityCrusher.java
+++ src/com/teammetallurgy/metallurgy/machines/crusher/TileEntityCrusher.java
@@ -2,20 +2,195 @@ package com.teammetallurgy.metallurgy.machines.crusher;
 
 import com.teammetallurgy.metallurgy.machines.TileEntityMetallurgy;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntityFurnace;
 
-public class TileEntityCrusher extends TileEntityMetallurgy
+public class TileEntityCrusher extends TileEntityMetallurgy implements ISidedInventory
 {
+    private static final int[] slots_top = new int[] { 0 };
+    private static final int[] slots_bottom = new int[] { 4, 3, 2, 1 };
+    private static final int[] slots_sides = new int[] { 1 };
+
+    private ItemStack[] itemStacks = new ItemStack[5];
+
+    public int crusherBurnTime;
+    public int currentItemBurnTime;
+    public int crusherCookTime;
 
     @Override
-    protected void writeCustomNBT(NBTTagCompound compound)
+    protected void writeCustomNBT(NBTTagCompound data)
     {
+        data.setShort("BurnTime", (short) this.crusherBurnTime);
+        data.setShort("CookTime", (short) this.crusherCookTime);
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < this.itemStacks.length; ++i)
+        {
+            if (this.itemStacks[i] != null)
+            {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte) i);
+                this.itemStacks[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+
+        data.setTag("Items", nbttaglist);
 
     }
 
     @Override
     protected void readCustomNBT(NBTTagCompound data)
     {
+        NBTTagList nbttaglist = data.getTagList("Items");
+        this.itemStacks = new ItemStack[this.getSizeInventory()];
 
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound nbttagcompound1 = (NBTTagCompound) nbttaglist.tagAt(i);
+            byte b0 = nbttagcompound1.getByte("Slot");
+
+            if (b0 >= 0 && b0 < this.itemStacks.length)
+            {
+                this.itemStacks[b0] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+            }
+        }
+
+        this.crusherBurnTime = data.getShort("BurnTime");
+        this.crusherCookTime = data.getShort("CookTime");
+        this.currentItemBurnTime = TileEntityFurnace.getItemBurnTime(this.itemStacks[1]);
+
+    }
+
+    @Override
+    public int getSizeInventory()
+    {
+        return itemStacks.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int i)
+    {
+        return itemStacks[i];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int i, int j)
+    {
+        if (this.itemStacks[i] != null)
+        {
+            ItemStack itemstack;
+
+            if (this.itemStacks[i].stackSize <= j)
+            {
+                itemstack = this.itemStacks[i];
+                this.itemStacks[i] = null;
+                return itemstack;
+            }
+            else
+            {
+                itemstack = this.itemStacks[i].splitStack(j);
+
+                if (this.itemStacks[i].stackSize == 0)
+                {
+                    this.itemStacks[i] = null;
+                }
+
+                return itemstack;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int i)
+    {
+        if (this.itemStacks[i] != null)
+        {
+            ItemStack itemstack = this.itemStacks[i];
+            this.itemStacks[i] = null;
+            return itemstack;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public void setInventorySlotContents(int i, ItemStack itemstack)
+    {
+        this.itemStacks[i] = itemstack;
+
+        if (itemstack != null && itemstack.stackSize > this.getInventoryStackLimit())
+        {
+            itemstack.stackSize = this.getInventoryStackLimit();
+        }
+    }
+
+    @Override
+    public String getInvName()
+    {
+        return "container.crusher";
+    }
+
+    @Override
+    public boolean isInvNameLocalized()
+    {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit()
+    {
+        return 64;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer entityplayer)
+    {
+        return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord, this.zCoord) != this ? false : entityplayer.getDistanceSq((double) this.xCoord + 0.5D, (double) this.yCoord + 0.5D, (double) this.zCoord + 0.5D) <= 64.0D;
+    }
+
+    @Override
+    public void openChest()
+    {
+    }
+
+    @Override
+    public void closeChest()
+    {
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int i, ItemStack itemstack)
+    {
+        return i >= 2 ? false : (i == 1 ? TileEntityFurnace.isItemFuel(itemstack) : true);
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int side)
+    {
+        return side == 0 ? slots_bottom : (side == 1 ? slots_top : slots_sides);
+    }
+
+    @Override
+    public boolean canInsertItem(int i, ItemStack itemstack, int j)
+    {
+        return this.isItemValidForSlot(i, itemstack);
+    }
+
+    @Override
+    public boolean canExtractItem(int i, ItemStack itemstack, int j)
+    {
+        return i != 0 || i != 1 || itemstack.itemID == Item.bucketEmpty.itemID;
     }
 }
