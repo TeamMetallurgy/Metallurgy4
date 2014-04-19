c06e7135f271d409e527dd38da496fdc4674bffa
freyjadono <mike.stengel@gmail.com>
2014-04-19 00:38:05 -0400

    ItemStacks make for bad hash, so using the unlocalized names for now

--- src/main/java/com/teammetallurgy/metallurgy/recipes/CrusherRecipes.java
+++ src/main/java/com/teammetallurgy/metallurgy/recipes/CrusherRecipes.java
@@ -5,33 +5,31 @@ import java.util.HashMap;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class CrusherRecipes
-{
-    private static CrusherRecipes instance = new CrusherRecipes();
+public class CrusherRecipes {
+	private static CrusherRecipes instance = new CrusherRecipes();
 
-    public static CrusherRecipes getInstance()
-    {
-        return CrusherRecipes.instance;
-    }
+	public static CrusherRecipes getInstance() {
+		return CrusherRecipes.instance;
+	}
 
-    private HashMap<ItemStack, ItemStack> metaList = new HashMap<ItemStack, ItemStack>();
+	private final HashMap<String, ItemStack> metaList = new HashMap<String, ItemStack>();
 
-    @Deprecated
-    public void addRecipe(int itemID, int itemDamage, ItemStack itemStack)
-    {
-        addRecipe(new ItemStack(Item.getItemById(itemID), 1, itemDamage), itemStack);
-    }
+	@Deprecated
+	public void addRecipe(int itemID, int itemDamage, ItemStack itemStack) {
+		this.addRecipe(new ItemStack(Item.getItemById(itemID), 1, itemDamage),
+				itemStack);
+	}
 
-    public ItemStack getCrushingResult(ItemStack itemStack)
-    {
-        if (itemStack == null) { return null; }
-        return this.metaList.get(itemStack);
+	public ItemStack getCrushingResult(ItemStack itemStack) {
+		if (itemStack == null) {
+			return null;
+		}
+		return this.metaList.get(itemStack.getUnlocalizedName());
 
-    }
+	}
 
-    public void addRecipe(ItemStack oreItem, ItemStack ret)
-    {
-        this.metaList.put(oreItem, ret);
-    }
+	public void addRecipe(ItemStack oreItem, ItemStack ret) {
+		this.metaList.put(oreItem.getUnlocalizedName(), ret);
+	}
 
 }
