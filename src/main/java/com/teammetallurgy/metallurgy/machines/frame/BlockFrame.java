df9d220c973118e66f2ac3e8c32d4cf70d7f86b4
Glassmaker <JeanGlassmaker@mail.com>
2014-04-22 20:33:38 +0300

    Added Machine Frame Textures

--- src/main/java/com/teammetallurgy/metallurgy/machines/frame/BlockFrame.java
+++ src/main/java/com/teammetallurgy/metallurgy/machines/frame/BlockFrame.java
@@ -1,22 +1,55 @@
 package com.teammetallurgy.metallurgy.machines.frame;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import com.teammetallurgy.metallurgy.machines.BlockMetallurgy;
 
 public class BlockFrame extends BlockMetallurgy
 {
+
+    private String botTexture = "metallurgy:machines/mframe_bottom";
+    private String topTexture = "metallurgy:machines/mframe_top";
+    private String sideTexture = "metallurgy:machines/mframe_side";
+
+    private IIcon botIcon;
+    private IIcon topIcon;
+    private IIcon sideIcon;
+
     @Override
     public TileEntity createNewTileEntity(World var1, int var2)
     {
         return null;
     }
-    
+
     @Override
     protected void doOnActivate(World world, int x, int y, int z, EntityPlayer player, int side, float xOffset, float yOffset, float zOffset)
     {
-        
+
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta)
+    {
+        switch (side)
+        {
+            case 0:
+                return this.botIcon;
+            case 1:
+                return this.topIcon;
+            default:
+                return this.sideIcon;
+        }
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister register)
+    {
+        this.botIcon = register.registerIcon(this.botTexture);
+        this.topIcon = register.registerIcon(this.topTexture);
+        this.sideIcon = register.registerIcon(this.sideTexture);
     }
 }
