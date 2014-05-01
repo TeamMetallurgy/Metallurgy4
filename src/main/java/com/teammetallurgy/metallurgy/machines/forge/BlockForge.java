573174f2e4a5adbeb02d9a75e0c0b9ab9ea65f51
Glassmaker <JeanGlassmaker@mail.com>
2014-05-01 17:56:47 +0300

    Added forge textures

--- src/main/java/com/teammetallurgy/metallurgy/machines/forge/BlockForge.java
+++ src/main/java/com/teammetallurgy/metallurgy/machines/forge/BlockForge.java
@@ -1,9 +1,13 @@
 package com.teammetallurgy.metallurgy.machines.forge;
 
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidContainerRegistry;
@@ -16,6 +20,17 @@ import com.teammetallurgy.metallurgy.machines.BlockMetallurgy;
 
 public class BlockForge extends BlockMetallurgy
 {
+    private String topTexture = "metallurgy:machines/smelter_top";
+    private String sideTexture = "metallurgy:machines/smelter_side";
+    private String sideConnectedTexture = "metallurgy:machines/smelter_side_connected";
+    private String frontTexture = "metallurgy:machines/smelter_front";
+    private String bottomTexture = "metallurgy:machines/smelter_bottom";
+
+    private IIcon topIcon;
+    private IIcon sideIcon;
+    private IIcon sideConnectedIcon;
+    private IIcon frontIcon;
+    private IIcon bottomIcon;
 
     public BlockForge()
     {
@@ -63,6 +78,59 @@ public class BlockForge extends BlockMetallurgy
         }
 
         return super.onBlockActivated(world, x, y, z, player, side, xOffset, yOffset, zOffset);
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, int coordX, int coordY, int coordZ, EntityLivingBase livingEntity, ItemStack blockItemStack)
+    {
+        int l = MathHelper.floor_double((double) (livingEntity.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            world.setBlockMetadataWithNotify(coordX, coordY, coordZ, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            world.setBlockMetadataWithNotify(coordX, coordY, coordZ, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            world.setBlockMetadataWithNotify(coordX, coordY, coordZ, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            world.setBlockMetadataWithNotify(coordX, coordY, coordZ, 4, 2);
+        }
+
+    }
+
+    @Override
+    public IIcon getIcon(int side, int meta)
+    {
+        if (side == meta) { return this.frontIcon; }
+
+        switch (side)
+        {
+            case 0:
+                return this.bottomIcon;
+            case 1:
+                return this.topIcon;
+            default:
+                return this.sideIcon;
+        }
+    }
+    
+    @Override
+    public void registerBlockIcons(IIconRegister register)
+    {
+        this.bottomIcon = register.registerIcon(this.bottomTexture);
+        this.topIcon = register.registerIcon(this.topTexture);
+        this.sideIcon = register.registerIcon(this.sideTexture);
+        this.frontIcon = register.registerIcon(this.frontTexture);
+        this.sideConnectedIcon = register.registerIcon(this.sideConnectedTexture);
     }
 
 }
