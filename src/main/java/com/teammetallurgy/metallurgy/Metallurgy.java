74aa5b3564ea1ab0b6744c315ca610f937539889
Glassmaker <JeanGlassmaker@mail.com>
2014-09-23 15:09:19 +0300

    Fixes misspelling in deps

--- src/main/java/com/teammetallurgy/metallurgy/Metallurgy.java
+++ src/main/java/com/teammetallurgy/metallurgy/Metallurgy.java
@@ -33,7 +33,7 @@ public class Metallurgy
     public static final String MODNAME = "Metallurgy";
     public static final String MODID = "Metallurgy";
     public static final String VERSION = "4.0.4";
-    public static final String DEPS = "reqired-after:MetallurgyCore@[4.0.3,];before:UndergroundBiomes;after:Botania;after:TConstruct";
+    public static final String DEPS = "required-after:MetallurgyCore@[4.0.3,];before:UndergroundBiomes;after:Botania;after:TConstruct";
 
     @Mod.Instance(Metallurgy.MODID)
     public static Metallurgy instance;
