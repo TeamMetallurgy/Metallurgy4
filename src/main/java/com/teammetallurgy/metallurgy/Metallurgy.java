986d1c465b7c6839b514ad813e9b3baa01b0193e
Glassmaker <JeanGlassmaker@mail.com>
2014-10-03 12:35:06 +0300

    Version bump to 4.0.5, forge to 1208, and Metallurgy core to 4.0.4

--- src/main/java/com/teammetallurgy/metallurgy/Metallurgy.java
+++ src/main/java/com/teammetallurgy/metallurgy/Metallurgy.java
@@ -32,8 +32,8 @@ public class Metallurgy
 {
     public static final String MODNAME = "Metallurgy";
     public static final String MODID = "Metallurgy";
-    public static final String VERSION = "4.0.4";
-    public static final String DEPS = "required-after:MetallurgyCore@[4.0.3,];before:UndergroundBiomes;after:Botania;after:TConstruct";
+    public static final String VERSION = "4.0.5";
+    public static final String DEPS = "required-after:MetallurgyCore@[4.0.4,];before:UndergroundBiomes;after:Botania;after:TConstruct";
 
     @Mod.Instance(Metallurgy.MODID)
     public static Metallurgy instance;
