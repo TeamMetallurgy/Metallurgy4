53ea753d5165e2cf4a804f707580935d3b9e9a4a
Glassmaker <JeanGlassmaker@mail.com>
2015-10-20 22:44:19 +0300

    Bumps Metallurgy's API version to 2.0.2

--- src/main/java/com/teammetallurgy/metallurgy/api/package-info.java
+++ src/main/java/com/teammetallurgy/metallurgy/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner="Metallurgy", apiVersion = "2.0.1", provides="MetallurgyAPI")
+@API(owner="Metallurgy", apiVersion = "2.0.2", provides="MetallurgyAPI")
 package com.teammetallurgy.metallurgy.api;
 
 import cpw.mods.fml.common.API;
